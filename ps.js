d15bf58d27a6507897cd7c1307728c12cfef3364
saga61981-sketch <saga61981@gmail.com>
2026-04-10 20:01:47 +0200

    1

--- ps.js
+++ ps.js
@@ -14,7 +14,7 @@ const authors = [
 
 // ===== BOOKS =====
 let books = [
-  {id:1,title:"Harry Potter",authorId:1,category:"Fantasy",price:19.99,rating:4.9,favorite:false,image:"https://covers.openlibrary.org/b/id/7984916-L.jp"},
+  {id:1,title:"Harry Potter",authorId:1,category:"Fantasy",price:19.99,rating:4.9,favorite:false,image:"https://covers.openlibrary.org/b/id/7984916-L.jpg"},
   {id:2,title:"The Great Gatsby",authorId:2,category:"Classic",price:14.50,rating:4.4,favorite:false,image:"https://covers.openlibrary.org/b/id/7352161-L.jpg"},
   {id:3,title:"1984",authorId:3,category:"Dystopian",price:16.00,rating:4.8,favorite:false,image:"https://covers.openlibrary.org/b/id/7222246-L.jpg"},
   {id:4,title:"Pride and Prejudice",authorId:4,category:"Romance",price:12.75,rating:4.6,favorite:false,image:"https://covers.openlibrary.org/b/id/8091016-L.jpg"},
